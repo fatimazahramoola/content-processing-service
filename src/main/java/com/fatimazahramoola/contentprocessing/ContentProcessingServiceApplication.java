eecddf13a7ab2562a8a3b7d0315deff8dc1f32c8
fatimazahramoola <fatimazahramoola@gmail.com>
2026-06-30 20:39:01 +0200

    Bootstrap Spring Boot application (#1)

---
+++ src/main/java/com/fatimazahramoola/contentprocessing/ContentProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fatimazahramoola.contentprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentProcessingServiceApplication.class, args);
+	}
+
+}
